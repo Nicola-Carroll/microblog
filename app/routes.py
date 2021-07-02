f5d744e9b431713e0a420318c65ec876cf7ed1be
nicola <nicolaccarroll@gmail.com>
2021-07-02 15:20:37 +0100

    added base HTML template

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,14 @@ from app import app
 @app.route('/index')
 def index():
     user = {'username': 'Captain Charlie Hook Hand'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username': 'Jimbo'},
+            'body': 'Excellent bounty find today lads :)'
+        },
+        {
+            'author': {'username': 'Susan (for a boy)'},
+            'body': 'The seas are restless tonight'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
